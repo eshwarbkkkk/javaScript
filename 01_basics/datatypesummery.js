dc7a463961dd6d67a95dfa1de5e7e1463a60cb58
ESHWAR <148619149+eshwarbkkkk@users.noreply.github.com>
2024-10-21 18:52:45 +0000

    dataConversion

---
+++ 01_basics/datatypesummery.js
@@ -0,0 +1,17 @@
+//premitive
+
+//7 types : String, Number, null, Boolean, undefined, Symbol, BigInt
+
+
+//
+//Reference or Non premitive data type
+// arrays, objects, functions
+const myArray=["shaktiman","Kalki","Hanuman"];
+//objects
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("hello world");
+}
